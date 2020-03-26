ed142fcb262b55ee8d37b60a2b58643346d5896b
MartynasCepas <martynas.cepas@ktu.edu>
2020-03-26 19:55:12 +0200

    Added service to get projects

--- src/main/java/com/martynascepas/ppmtool/services/ProjectService.java
+++ src/main/java/com/martynascepas/ppmtool/services/ProjectService.java
@@ -5,6 +5,7 @@ import com.martynascepas.ppmtool.exceptions.ProjectIdException;
 import com.martynascepas.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 public class ProjectService {
@@ -21,6 +22,23 @@ public class ProjectService {
         }catch (Exception e){
             throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase()+"' already exists.");
         }
-
     }
+
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project ID '" + projectId.toUpperCase() +"' does not exists.");
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+
+
 }
