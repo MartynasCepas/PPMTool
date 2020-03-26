02f596c7d7c5d21e450abd08d9888c8eaf349632
MartynasCepas <martynas.cepas@ktu.edu>
2020-03-26 20:25:47 +0200

    Added delete service

--- src/main/java/com/martynascepas/ppmtool/web/ProjectController.java
+++ src/main/java/com/martynascepas/ppmtool/web/ProjectController.java
@@ -50,4 +50,11 @@ public class ProjectController {
         return projectService.findAllProjects();
     }
 
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId){
+        projectService.deleteProjectByIdentifier(projectId.toUpperCase());
+
+        return new ResponseEntity<String>("Project with ID: '" + projectId + "' was deleted.", HttpStatus.OK);
+    }
+
 }
